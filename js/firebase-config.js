442e3c8007972f37c42ea0377ae159b045c058ed
Prof-Marcio <marcio2862@gmail.com>
2026-02-17 18:01:29 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,10 @@
-// Importações Firebase (versão modular moderna)
+// js/firebase-config.js
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// SUA CONFIGURAÇÃO
 const firebaseConfig = {
-  apiKey: "AIzaSyAn1Eja_qa81xRhzn3lYy_X7t7ML2DKxkg",
+  apiKey: "SUA_API_KEY_AQUI",
   authDomain: "mestre-360.firebaseapp.com",
   projectId: "mestre-360",
   storageBucket: "mestre-360.firebasestorage.app",
@@ -13,9 +12,5 @@ const firebaseConfig = {
   appId: "1:894952532346:web:881d00bdc0f6ffd6a38ec3"
 };
 
-// Inicializa
 const app = initializeApp(firebaseConfig);
-
-// Exporta
 export const auth = getAuth(app);
-export const db = getFirestore(app);
