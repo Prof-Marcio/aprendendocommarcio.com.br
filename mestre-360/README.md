292c43f9517f7f9fdd8ac3fdebb7f8d60c50fee7
Prof-Marcio <marcio2862@gmail.com>
2026-02-18 17:02:46 -0300

    0.1

--- mestre-360/README.md
+++
@@ -1,7 +0,0 @@
-# Olá Mundo!
-Primeiro Repositório Remoto teste
-
-Aprendir no Curso em Video do Gustava Guanabara.
-
-
-Testando edição online no próprio git
---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".btn").forEach(botao => {
+    botao.addEventListener("click", () => {
+        alert("Função ainda em desenvolvimento - versão 0.1");
+    });
+});
